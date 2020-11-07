0ccdfc2f22b30c71ca7f1320d85e530eece0236a
feat7 <vinaykhobragade99@gmail.com>
2020-11-07 21:12:12 +0530

    add: more comments and exceptions

--- exceptions.py
+++ exceptions.py
@@ -3,3 +3,10 @@ class BadCSVFile(Exception):
     Error occured while reading the csv file
     """
     pass
+
+
+class BadQueryParameter(Exception):
+    """
+    Error occured because query list is empty
+    """
+    pass
--- faqengine.py
+++ faqengine.py
@@ -1,63 +1,77 @@
 import pandas as pd
+import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from exceptions import *
 
 
 class FAQEngine:
-    def __init__(self, csv_file: str):
+
+    def __init__(self, csv_file: str = 'faqs.csv'):
         """
-        FAQEngine constructor
-        Takes csv_file: str as input (path)
-        CSV should contain ['Question', 'Answer'] columns
+        FAQEngine Constructor
+        Takes csv_file: str (input path of csv file)
+        CSV file should contain ['Question', 'Answer'] columns
         """
         self.data = self.parse_csv_file(csv_file)
-        self.model = self.build_model()
-
-    def get_vector(self, query: list):
-        """
-        Returns a vector for the given query using the vectorizer method that is used to init the FAQEngine class.
-        """
-        return self.vectorizer.transform(query)
-
-    def build_model(self):
-        documents = self.data['Question']
-        self.vectorizer = TfidfVectorizer(stop_words='english').fit(documents)
-        self.vectors = self.vectorizer.transform(documents)
-
-    # TODO:
-    def preprocess_data(self):
-        pass
+        self.build_model(self.data['Question'])
 
     def parse_csv_file(self, csv_file: str):
         """
-        CSV file should contain two columns: Question, Answer
-        Remove all NaN values
+        Reads the csv file
+        CSV file should contain ['Question', 'Answer'] columns
+        Remove NaN values
+        Throw error if format is bad or file does not exist
         """
         try:
             df = pd.read_csv(csv_file)
+
             if not set(['Question', 'Answer']).issubset(df.columns):
                 raise BadCSVFile(
                     "CSV file does not contain ['Question', 'Answer'] columns.")
+
             df.dropna(inplace=True)
 
         except Exception as e:
-            raise BadCSVFile("Error reading csv file.")
+            raise BadCSVFile(
+                "Error while reading the csv file. Please check the path of the file or the file might be curropted.")
 
         return df
 
-    def get_cosine_similarity(self, query: list):
-        return cosine_similarity(
-            self.get_vector(query), self.vectors).flatten()
+    def build_model(self, documents):
+        """
+        Create a mechanism to vectorize the data
+        Create vector for our current dataset
+        """
+        self.vectorizer = TfidfVectorizer(
+            stop_words='english', lowercase=True).fit(documents)
+        self.vectors = self.vectorizer.transform(documents)
 
-    # TODO:
-    def distance_similarity(self):
-        pass
+    def get_vector(self, query: list):
+        """
+        Returns a vector for a given query
+        """
+        if len(query) == 0:
+            raise BadQueryParameter("Query (list) can not be empty.")
+
+        return self.vectorizer.transform(query)
+
+    def get_cosine_similarity(self, query: list):
+        """
+        Returns cosine similarity between the input question and our dataset questions
+        """
+        question_vector = self.get_vector(query)
+
+        return cosine_similarity(question_vector, self.vectors).flatten()
 
     def perform_query(self, query: str, n: int = 5):
+        """
+        API function that can be used by other codebase
+        It takes query (string) and n top similar questions along with thier answers
+        """
         query = [query]
         cosine_similarities = self.get_cosine_similarity(query)
-        related_document_indices = cosine_similarities.argsort()[:-n:-1]
+        indices = cosine_similarities.argsort()[:-n-1:-1]
         return self.data[
-            self.data.index.isin(related_document_indices.tolist())
+            self.data.index.isin(indices)
         ].reset_index(drop=True).to_dict(orient='records')
